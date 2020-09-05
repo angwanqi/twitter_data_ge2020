707b9b0004743a617f215981c4d6ec189062febe
wqang <42666676+angwanqi@users.noreply.github.com>
2020-09-05 15:07:42 +0800

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,7 @@
+# Config File to store API creedentials
+# Replace with your own API credentials
+
+CONSUMER_KEY = '<CONSUMER_KEY>'
+CONSUMER_SECRET = '<CONSUMER_SECRET>'
+ACCESS_KEY = '<ACCESS_KEY>'
+ACCESS_SECRET = '<ACCESS_SECRET>'
